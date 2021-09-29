38b873fbbdd57d3c41849148c368a87fd3ab095c
Alejandro Largo <largoalejo@gmail.com>
2021-09-28 23:33:47 -0500

    se eliminaron comentarios

--- 3/index.js
+++ 3/index.js
@@ -1,6 +1,4 @@
 const str = 'hello world, big WoRld';
-// const str =
-//   'Hola que tal soy el chico de las poesias que tal hola hola que tal soy el poesias chico chico';
 const re = /[^A-Za-z\s]/;
 const newStr = str.toLowerCase().replace(re, '').split(' ');
 
